a4a0acbae4a27ad4bf346f7fa4a0013c47a9e125
Manu <manuelalejandrojuarez@gmail.com>
2021-12-14 16:01:32 +0000

    Version with more work in Jupyter notebook and first version of geocoding with geopy

--- zoopla_scraper/zoopla_scraper/spiders/zoopla_spider.py
+++ zoopla_scraper/zoopla_scraper/spiders/zoopla_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import re
 from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 
 class ZooplaSpider(scrapy.Spider):
     name='zoopla'
@@ -12,7 +13,9 @@ class ZooplaSpider(scrapy.Spider):
     
     def find_coordinates(self,address):
         geolocator=Nominatim(user_agent='manu')
-        location=geolocator.geocode(address)
+        geocode=RateLimiter(geolocator.geocode,min_delay_seconds=1)
+        if address:
+            location=geocode(address)
 
         if location:
             latitude=location.latitude
@@ -26,23 +29,23 @@ class ZooplaSpider(scrapy.Spider):
 
     def parse_property(self,response):
         
-        title=response.xpath('//span[@class="css-1dfc15y-DisplayTitleLabel ep4jli5"]/text()').get()
-        address=response.xpath('//span[@class="css-1o06bum-DisplayAddressLabel ep4jli4"]/text()').get()
-        price=response.xpath('//span[@class="css-dob1au-PricingLabel ep4jli10"]/text()').get()
+        title=response.xpath('//span[@class="css-1dfc15y-DisplayTitleLabel eiwe0nt5"]/text()').get()
+        address=response.xpath('//span[@class="css-1o06bum-DisplayAddressLabel eiwe0nt4"]/text()').get()
+        price=response.xpath('//span[@class="css-dob1au-PricingLabel eiwe0nt10"]/text()').get()
         description=response.xpath('//div[@class="css-1sui95d-RichText ep7k4g50"]/span/text()').getall()
-        features=response.xpath('//ul[@class="css-1ibzupy-BulletList-FeaturesList ei8ghzj2"]/li/text()').getall()
-        number_of_beds= response.xpath('//span[(@data-testid="beds-label") and (@class="css-8rvu8h-AttributeLabel ep4jli0")]/text()').get()
-        number_of_baths=response.xpath('//span[(@data-testid="baths-label") and (@class="css-8rvu8h-AttributeLabel ep4jli0")]/text()').get()
+        features=response.xpath('//ul[@class="css-1ibzupy-BulletList-FeaturesList edyyq052"]/li/text()').getall()
+        number_of_beds= response.xpath('//span[(@data-testid="beds-label") and (@class="css-8rvu8h-AttributeLabel eiwe0nt0")]/text()').get()
+        number_of_baths=response.xpath('//span[(@data-testid="baths-label") and (@class="css-8rvu8h-AttributeLabel eiwe0nt0")]/text()').get()
         images=response.xpath('//div[@class="css-1i4h94a-SlideImageWrapper e16xseoz0"]/picture/source/@srcset').get()
         images=re.findall('https://lid.zoocdn.com/u/\d+/\d+/\w+\.jpg:p',images)
 
         letting_agent_name=response.xpath('//p[@class="css-1g2z706-Text-AgentName e1swwt8d3"]/text()').get()
-        available_from=response.xpath('//span[@class="css-1f6ruxg-AvailableFrom ep4jli1"]/text()').get()
+        available_from=response.xpath('//span[@class="css-1f6ruxg-AvailableFrom eiwe0nt1"]/text()').get()
 
-        #latitude,longitude=self.find_coordinates(address)
-        #'latitude':latitude,'longitude':longitude,
+        latitude,longitude=self.find_coordinates(address)
+       
         
-        yield {'title':title,'address':address,'price':price,'description':description,'features':features,'number_beds':number_of_beds,
+        yield {'title':title,'address':address,'latitude':latitude,'longitude':longitude,'price':price,'description':description,'features':features,'number_beds':number_of_beds,
         'number_baths':number_of_baths,'images':images,'letting_agent_name':letting_agent_name,'available_from':available_from}
         
 
